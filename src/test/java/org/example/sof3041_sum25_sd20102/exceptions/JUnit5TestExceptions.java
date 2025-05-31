488b91e6a9e9448cdcfb05e122de1484e526c039
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-05-31 11:07:44 +0700

    Basics: assertThrows()

---
+++ src/test/java/org/example/sof3041_sum25_sd20102/exceptions/JUnit5TestExceptions.java
@@ -0,0 +1,29 @@
+package org.example.sof3041_sum25_sd20102.exceptions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JUnit5TestExceptions {
+
+    @Test
+    @DisplayName("Exceptions test")
+    void test() {
+        String str = null;
+        assertThrows(NullPointerException.class, () -> str.length());
+
+        Foo foo = new Foo();
+        Exception exception = assertThrows(Exception.class, () -> foo.foo());
+
+        assertEquals("Exception Message", exception.getMessage());
+    }
+
+}
+
+class Foo {
+    void foo() throws Exception {
+        throw new Exception("Exception Message");
+    }
+}
