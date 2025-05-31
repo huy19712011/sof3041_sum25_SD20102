5f525f11549f560507e1be4abacf1c489b17ee7b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-05-31 09:49:36 +0700

    set up spring boot project

---
+++ src/main/java/org/example/sof3041_sum25_sd20102/Sof3041Sum25Sd20102Application.java
@@ -0,0 +1,15 @@
+package org.example.sof3041_sum25_sd20102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3041Sum25Sd20102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3041Sum25Sd20102Application.class, args);
+        System.out.println("Oki");
+
+    }
+
+}
---
+++ src/test/java/org/example/sof3041_sum25_sd20102/Sof3041Sum25Sd20102ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sof3041_sum25_sd20102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3041Sum25Sd20102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
